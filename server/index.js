f2d7c2015d80dca9b42c32ef11f8a5cb871f5a80
Gavindu <pabothavindu@gmail.com>
2026-07-18 15:36:53 +0530

    Updated backend for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -390,7 +390,4 @@ app.post('/files/list', async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
